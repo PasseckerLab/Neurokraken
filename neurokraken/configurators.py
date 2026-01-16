61e05dc2080ee8fc233ba0d5337683325bec456f
alexanderwallerus <alex.wallerus@gmail.com>
2026-01-16 17:06:57 +0100

    configurators docstrings

--- neurokraken/configurators.py
+++ neurokraken/configurators.py
@@ -144,10 +144,13 @@ class _Devices:
         """A digital read of a pin (HIGH or Low), (True or False). Many devices can provide a suitable
         input to an arduino pin from simple buttons to integrated touch sensors. Provides 0 or 1
         upon get.read_in(<device name>).
-        logging:bool - log changes in value for future usage. Defaults to True.
-        Application Example: 
-        serial_in = {'licking_sensor': true_false_sensor(pin=3)}
-        print(get.read_in('licking_sensor')) # 0 or 1
+
+        Args:
+            logging:bool - log changes in value for future usage. Defaults to True.
+
+        Example:
+            >>> serial_in = {'licking_sensor': true_false_sensor(pin=3)}
+            >>> print(get.read_in('licking_sensor')) # 0 or 1
         """
         return {'value': 0, 'encoding': 'uint', 'byte_length': 1, 'logging': logging,
                 'arduino_class': 'DigitalSensor', 'arduino_args': pin,
@@ -158,10 +161,13 @@ class _Devices:
                     keys_control=lambda keys : 256 if keys[0] else 768 if keys[1] else 512):
         """Analog read of a pin. Provides a continuous value 0 to 1023 upon get.read_in(<device name>).
         Analog reads can be used with devices like an angle measuring potentiometer or light sensitive photoresistor.
-        logging:bool - log changes in value for future usage. Defaults to True.
-        Application Example:
-        serial_in = {'rotation_potentiometer': continuous_sensor(pin=3)}
-        print(get.read_in('rotation_potentiometer')) # 387
+        
+        Args:
+            logging:bool - log changes in value for future usage. Defaults to True.
+        
+        Example:
+            >>> serial_in = {'rotation_potentiometer': continuous_sensor(pin=3)}
+            >>> print(get.read_in('rotation_potentiometer')) # 387
         """
         return {'value': 0, 'encoding': 'uint', 'byte_length': 2, 'logging': logging,
                 'arduino_class': 'AnalogSensor', 'arduino_args': pin,
@@ -174,16 +180,17 @@ class _Devices:
         get.read_in(<device name>).
         Rotary encoders are useful for high precision low friction rotation readings like on a
         steering wheel or treading wheel/disc.
+        
         Args:
             logging:bool - log changes in value for future usage. Defaults to True.
             controls:bool - return the optional control entry for serial_out instead of the sensor_value for
                             serial_in. When adding the control running get.send_out(<device_name>) allows
                             resetting the wheel position to 0. Defaults to False
-        Application Example:
-        serial_in = {'wheel_pos': rotary_encoder(pins=[3,4])}
-        serial_out = {'reset_wheel': rotary_encoder(pint=[3,4], controls=True)} # optional
-        print(get.read_in('wheel_pos')) # -5738
-        get.send_out('reset_wheel', 1) # reset the wheel position to 0 (only if control provided to serial_out)
+        Example:
+        >>> serial_in = {'wheel_pos': rotary_encoder(pins=[3,4])}
+        >>> serial_out = {'wheel_pos': rotary_encoder(pins=[3,4], controls=True)} # optional
+        >>> print(get.read_in('wheel_pos')) # -5738
+        >>> get.send_out('wheel_pos', True) # reset the wheel position to 0 (only if control provided to serial_out)
         """
         if not controls:
             return {'value': 0, 'encoding': int, 'byte_length': 4, 'logging': logging,
@@ -200,14 +207,13 @@ class _Devices:
         The range depends on the resistors used with the connection. See documentation for example wiring.
 
         Capacitance sensing can be used as an easy way to detect touch or even proximity.
+        
         Args:
             logging:bool - log changes in value for future usage. Defaults to True.
         
-        Application Example:
-        
-        serial_in = {'touch': capacitive_touch(pins=[3,4])}
-        
-        print(get.read_in('touch')) # 481
+        Example:
+            >>> serial_in = {'touch': capacitive_touch(pins=[3,4])}
+            >>> print(get.read_in('touch')) # 481
         """
         return {'value': 0, 'encoding': int, 'byte_length': 4, 'logging': logging,
                 'arduino_class': 'CapacitiveRead', 'arduino_args': pins,
@@ -228,15 +234,17 @@ class _Devices:
         counting time when the neurokraken is started/active and until it is stopped/inactive.
         At inactive state all pins will be HIGH. Upon get.start() pulse clock pins start at 0.
         
-        Application example:
+        Args:
+            change_period:int - period in milliseconds for the pulse clock to switch HIGH/LOW
+            logging:bool - log changes in value for future usage. Defaults to True.
 
-        serial_in = {
-                     'clock_100ms': pulse_clock(2,     100),
-                     'clock_1s':    pulse_clock(3,    1000),
-                     'clock_10s':   pulse_clock(4,  10_000),
-                     'clock_10min': pulse_clock(5, 300_000)}
-        
-        print(get.read_in('clock_1s')) # 1 => This pin is currently HIGH 1 and not LOW 0"""
+        Example:
+        >>> serial_in = {'clock_100ms': pulse_clock(2,     100),
+        >>>              'clock_1s':    pulse_clock(3,    1000),
+        >>>              'clock_10s':   pulse_clock(4,  10_000),
+        >>>              'clock_10min': pulse_clock(5, 300_000)}
+        >>> ...
+        >>> print(get.read_in('clock_1s')) # 1 => This pin is currently HIGH 1 and not LOW 0"""
         return {'value': 0, 'encoding': 'uint', 'byte_length': 1, 'logging': logging,
                 'arduino_class': 'PulseClock', 'arduino_args': [pin, change_periods_ms]}
 
@@ -249,25 +257,32 @@ class _Devices:
         Manually adding time_millis(logging=True) can be useful as logging every noted change of the 
         current millisecond cam confirm neurokraken sensors being captured at millisecond precision 
         when using computer or arduino setups with a heavy compute load.
-        Application example:
-        serial_in = {'t_ms': _time_millis(logging=True)}
-        print(get.read_in('t_ms')) # 61_673"""
+        
+        Args:
+            logging:bool - log changes in value for future usage. Defaults to True.
+
+        Example:
+            >>> serial_in = {'t_ms': _time_millis(logging=True)}
+            >>> print(get.read_in('t_ms')) # 61_673"""
         return {'value': 0, 'encoding': 'uint', 'byte_length': 4, 'logging': logging,
                 'arduino_class': 'MillisReader'}
     
     def _time_micros(self, logging=False):
         """The current time in microsends. This sensor is generally not useful as the networker
-        is sampling every 1 millisecond but can be used for debugging purposes with the direct
-        networker mode.
-        Application example:
-        serial_in = {'t_us: _time_micros()}
-        print(get.read_in('t_us')) # 61_673_226"""
+        is sampling every 1 millisecond but can be used to performance test the precise time of value sampling.
+
+        Args:
+            logging:bool - log changes in value for future usage. Defaults to True.
+
+        Example:
+            >>> serial_in = {'t_us: _time_micros()}
+            >>> print(get.read_in('t_us')) # 61_673_226"""
         return {'value': 0, 'encoding': 'uint', 'byte_length': 4, 'logging': logging,
                 'arduino_class': 'MicrosReader'}
     
     # Controls
     
-    def direct_on(self, pin:int, start_value=False, default_value=False):
+    def direct_on(self, pin:int, start_value=False):
         """Directly turn a pin HIGH (i=1) or LOW (i=0) upon get.send_out(<device_name>, i)
         This can be used to control valves, buzzers, LEDs, ... anything you can
         control with a electric logic signal. These devices can also be time-limited
@@ -275,31 +290,35 @@ class _Devices:
         
         Args:
            pin (int): the teensy pin wired to the controlled device
+           start_value (bool): The initial value of the pin. Defaults to False (LOW).
 
         Example:
             >>> serial_out = {'odor_valve': direct_on_off(pin=3)}
             >>> get.send_out('odor_valve', 0) # turn the signal off / shut down the valve/LED/buzzer/...
         """
         return {'value': start_value, 'encoding': bool, 'byte_length': 1,
-                'default': default_value, 'reset_after_send': False,
+                'default': start_value, 'reset_after_send': False,
                 'arduino_class': 'DirectOn', 'arduino_args': pin}
     
     def timed_on(self, pin:int):
-        """Similar to direct_on_off for the same devices, but only turns the digital signal high
+        """Similar to direct_on_off for the same devices, but only turns the digital signal HIGH
         for the provided milliseconds upon get.send_out(<device_name>, <milliseconds>) before
         turning the signal LOW again. This can be used to easily provide well timed stimulus 
         lengths and reward sizes. The providable millisecond range is 0 to 65536.
-        Application Example:
-        serial_out = {'reward_valve': timed_on(pin=3)}
-        get.send_out('reward_valve', 60) # open the valve for 60 milliseconds
+        
+        Args:
+           pin (int): the teensy pin wired to the controlled device
+
+        Example:
+            >>> serial_out = {'reward_valve': timed_on(pin=3)}
+            >>> get.send_out('reward_valve', 60) # open the valve for 60 milliseconds
         """
         return {'value': 0, 'encoding': 'uint', 'byte_length': 2,
                 'default': 0, 'reset_after_send': True,
                 'arduino_class': 'TimedOn', 'arduino_args': pin}
     
     def tone(self, pin:int):
-        """
-        Set the tone frequency to be output by a passive buzzer or piezo device.
+        """Set the tone frequency to be output by a passive buzzer or piezo device.
         This device uses the arduino tone function. Send 0 to not provide a tone.
         The maximum providable frequency is 65_535.
 
@@ -317,8 +336,15 @@ class _Devices:
     def servo(self, pin:int):
         """A servo motor to dynamically move experiment elements upon get.send_out(<device_name>, angle)
         The angle value can range from 0 to 255.
-        Application example
-        serial_out = {'servo': servo(3)}
+        This device can also be used to control the strength of an LED.
+
+        Args:
+           pin (int): the teensy pin connected to the servo's control wire. Has to be a supportive pin from 0-15, 18-19, 22-25, 28-29, 33, 36-37 
+
+        Example
+            >>> serial_out = {'servo': servo(3)}
+            >>> ...
+            >>> get.send_out('servo', 100)
         """
         return {'value': 127, 'encoding': 'uint', 'byte_length': 1,
                 'default': 127, 'reset_after_send': False,
@@ -332,10 +358,7 @@ class _Devices:
         
         A special control for starting and stopping the task's experiment time.
         This control is required by neurokraken to control the teensy time with the key serial_out["start_stop"]
-        and thus auto-added to serial_out if no entry ["start_stop"] is fonud to already exist. 
-        Application Example:
-        serial_out = {'start_stop': {'value': 0, 'encoding': 'uint', 'byte_length': 1,
-                                     'default': 0, 'reset_after_send': True}}
+        and thus auto-added to serial_out if no entry ["start_stop"] is fonud to already exist.
         """
         return {'value': 0, 'encoding': 'uint', 'byte_length': 1, 
                 'default': 0, 'reset_after_send': True,
